1282fb33d8fc10f3634f4cc8c8a0ea417b5ba258
Mike Ramage <mike.ramage@gmail.com>
2024-07-15 21:58:05 +0100

    Slightly sad part 2

--- src/day16.rs
+++ src/day16.rs
@@ -12,7 +12,7 @@ pub fn day16() -> (usize, usize) {
         .collect::<Vec<isize>>();
 
     let part1: usize = (0..100)
-        .fold(signal, |acc, _| do_one_phase(&acc, &BASE))
+        .fold(signal.clone(), |acc, _| do_one_phase(&acc, &BASE))
         .iter()
         .map(|d| d.to_string())
         .take(8)
@@ -20,7 +20,58 @@ pub fn day16() -> (usize, usize) {
         .parse::<usize>()
         .unwrap();
 
-    (part1, 0)
+    //For part 2, my starting index is 5,974,057. The matrix of patterns is lower-triangular (all zeros in
+    // the bottom left, with num zeros for row i = i (assuming 0-indexing)). By row 5,974,057 all the non-zero
+    //entries from 5,974,057 to 6,500,000 are 1. As a result applying one phase to the digits from indices
+    //5,974,057 to 6,500,000 simply sums all those digits. We just do this 100 times. Naively
+    //that's 500,001*500,000/2 * 100 operations = trillions of operations. But non-naively, once we do it for
+    //5,974,057 we can get the values for each of the following digits by subtracting off the previous digit.
+    //So to calculate output digit with index 5,974,058, we take the previouly calculated value for 5,974,057
+    //and subtract the input value at index 5,974,057. That's only
+    //1,000,000 operations per phase. 100,000,000 should be more than manageable so let's not oversimplify further.
+    let mut new_signal = signal.repeat(10_000);
+    let initial_index = signal[0..7]
+        .iter()
+        .map(|d| d.to_string())
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap();
+
+    println!("Initial index:{}", initial_index);
+
+    //100 phases - let's do some old skool for loops as I don't currently have the energy to use functional style
+    //Note - it would have been neater to build up from the bottom, while my algorithm of adding them all up then subtracting
+    //one at a time is all a wee bit sad and twice as much work as it should have been.
+    for _i in 0..100 {
+        let mut running_value = 0; //initially built up as the new value of the initial index, but
+                                   //reduced by the value of the kth index for each subsequent index ot give the new kth index
+
+        #[allow(clippy::needless_range_loop)]
+        for j in (initial_index)..new_signal.len() {
+            running_value += new_signal[j];
+        }
+
+        let mut old_value = new_signal[initial_index];
+        new_signal[initial_index] = running_value.abs() % 10;
+
+        #[allow(clippy::needless_range_loop)]
+        for k in (initial_index + 1)..new_signal.len() {
+            //Value is the value at the initial index minus the current value of the kth element
+            running_value -= old_value;
+            old_value = new_signal[k];
+            new_signal[k] = running_value.abs() % 10;
+        }
+    }
+
+    //Think that's it. Now just output values initial_index to initial_index + 8 and return
+    let part2 = new_signal[initial_index..(initial_index + 8)]
+        .iter()
+        .map(|d| d.to_string())
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap();
+
+    (part1, part2)
 }
 
 fn do_one_phase(input: &[isize], base: &[isize]) -> Vec<isize> {
